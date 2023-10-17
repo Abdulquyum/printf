4d7b13b0c0114b8e35bafd231d8fe5d2d2c0c9ea
Adenekan Emmanuel <emmanueladenekan20@gmail.com>
2023-10-17 07:24:38 -0700

    sil

---
+++ convert_to_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * convert_to_bin - convert number to binary (base 2)
+ *   *
+ *    * @n: argument to be taken
+ *     *
+ *      * Return: 0
+ *       */
+int convert_to_bin(int n)
+{
+		if (n <= 0)
+					return (0);
+
+			convert_to_bin(n / 2);
+				_putchar(n % 2 + '0');
+					return (0);
+}
