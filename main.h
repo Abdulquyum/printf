42fd973aa103c062016a2c28a7a3154c895d01ad
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-21 03:29:41 +0100

    custom printf function

--- main.h
+++ main.h
@@ -9,7 +9,9 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_string(char *str);
-int print_int(int n);
-int convert_to_bin(int n);
+int print_int(unsigned int n);
+int convert_to_bin(unsigned int n);
+int to_hexadec_high(unsigned int n);
+int to_hexadec_low(unsigned int n)
 
 #endif
