ae48f87a5c8b5f348a96079ac2abcfd4466195cc
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-21 03:20:47 +0100

    custom printf function

--- _printf.c
+++ _printf.c
@@ -9,49 +9,97 @@
  *
  * Return:Number of character printed excluding null byte
  */
-int _printf(const char *format, ...)
+int _printable(const char *format, va_list printable)
 {
-	int _return = 0, iterate;
-	char a;
-
-	va_list printable;
-
-	va_start(printable, format);
+	int iterate, _return = 0, buff_ind = 0;
+	char buffer[BUFFSIZE];
 
 	for (iterate = 0; format[iterate] != '\0'; iterate++)
 	{
 		if (format[iterate] != '%')
 		{
-			_putchar(format[iterate]);
+			buffer[buff_ind] = _putchar(format[iterate]);
+			if (buff_ind == BUFFSIZE)
+				write(1, buffer, buff_ind);
 			_return += 1;
-		}	
+
+		}
+
 		else if (format[iterate] == '%')
-		{
+		{	
 			if (format[iterate + 1] == 'c')
 			{
-				_putchar(va_arg(printable, int));
+				buffer[buff_ind] = _putchar(va_arg(printable, int));
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);
 				_return += 1;
 
-			
 			}
 			else if (format[iterate + 1] == 's')
 			{
-				print_string(va_arg(printable, char *));
+				buffer[buff_ind] = print_string(va_arg(printable, char *));
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);
 				_return += 1;
 
-			
 			}
 			else if (format[iterate + 1] == '%')
 			{
-				a = '%';
-				_putchar(a);
+				buffer[buff_ind] = _putchar('%');
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);
 				_return += 1;
 			}
+			if (format[iterate + 1] == 'd' || format[iterate + 1] == 'i')
+			{
+				buffer[buff_ind] = print_int(va_arg(printable, int));
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);	
+				_return++;
+			}
+			else if (format[iterate + 1] == 'b')
+			{
+				buffer[buff_ind] = convert_to_bin(va_arg(printable, int));
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);
+				_return += 1;
+			}
+			else if (format[iterate + 1] == 'u')
+			{
+				buffer[buff_ind] = handle_unsigned(va_arg(printable, int));
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);
+				_return++;
+			}
+			else if (format[iterate + 1] == 'o')
+			{
+				buffer[buff_ind] = handle_octal(va_arg(printable, int));
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);
+				_return += 1;
+			}
+			else if (format[iterate + 1] == 'x')
+			{
+				buffer[buff_ind] = to_hexadec_low(va_arg(printable, int));
+				if (buff_ind == BUFFSIZE)
+					write(1, buffer, buff_ind);
+			}
+			else if (format[iterate + 1] == 'X')
+                        {
+                                buffer[buff_ind] = to_hexadec_high(va_arg(printable, int));
+                                if (buff_ind == BUFFSIZE)
+                                        write(1, buffer, buff_ind);
+                        }
 			iterate++;
 		}
 	}
 
-       	va_end(printable);
+	if (buffer[buff_ind] > 0)
+	{
+		write(1, format, buff_ind);
+	}
+
+	va_end(printable);
 
 	return (_return);
 }
