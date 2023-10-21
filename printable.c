e070188ece4ec721b0ef984da72d23a4da1f9480
Abdulquyum <ajumobiabdulquyum@gmail.com>
2023-10-21 03:00:06 +0100

    custom printf function

--- printable.c
+++ printable.c
@@ -3,62 +3,49 @@
 #include <stdlib.h>
 
 /**
- * _printf - prints like original printf function
+ * _printable - prints like original printf function
  *
  * @format: compulsory argument
+ * @printable: printable parameters
  *
- * Return:Number of character printed excluding null byte
+ * Return: Number of character printed excluding null byte
  */
 int _printable(const char *format, va_list printable)
 {
-	int iterate, _return = 0, buff_ind = 0;
-	char buffer[BUFFSIZE];
+	int iterate, _return = 0;
 	char a;
 
 	for (iterate = 0; format[iterate] != '\0'; iterate++)
 	{
 		if (format[iterate] != '%')
 		{
-			buffer[buff_ind] = _putchar(format[iterate]);
-			if (buff_ind == BUFFSIZE)
-				write(1, buffer, buff_ind);
+			_putchar(format[iterate]);
 			_return += 1;
 
 		}
 
 		else if (format[iterate] == '%')
-		{	
+		{
 			if (format[iterate + 1] == 'c')
 			{
-				buffer[buff_ind] = _putchar(va_arg(printable, int));
-				if (buff_ind == BUFFSIZE)
-					write(1, buffer, buff_ind);
+				_putchar(va_arg(printable, int));
 				_return += 1;
 
 			}
 			else if (format[iterate + 1] == 's')
 			{
-				buffer[buff_ind] = print_string(va_arg(printable, char *));
-				if (buff_ind == BUFFSIZE)
-					write(1, buffer, buff_ind);
+				print_string(va_arg(printable, char *));
 				_return += 1;
 
 			}
 			else if (format[iterate + 1] == '%')
 			{
 				a = '%';
-				buffer[buff_ind] = _putchar(a);
-				if (buff_ind == BUFFSIZE)
-					write(1, buffer, buff_ind);
+				_putchar(a);
 				_return += 1;
 			}
 			iterate++;
 		}
-	}
-
-	if (buffer[buff_ind] > 0)
-	{
-		write(1, format, buff_ind);
 	}
 
 	va_end(printable);
